ce05f5085ab51b42c0895f906860a99022501be0
Keo-PC\Keo <keonakhon.kawan@outlook.com>
2017-09-08 14:15:51 +0700

    update: run 'before', before run test if successful

--- udemy-tutorial/01-users/test/test_helper.js
+++ udemy-tutorial/01-users/test/test_helper.js
@@ -2,12 +2,20 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/user_test');
-mongoose.connection
-  .once('open', () => {})
-  .on('error', (error) => {
-    console.warn('Warninng', error);
-  });
+/*before((done) => {
+  mongoose.connect('mongodb://localhost/user_test');
+  mongoose.connection
+    .once('open', () => { done(); })
+    .on('error', (error) => {
+      console.warn('Warninng', error);
+    });
+});*/
+
+before((done)=>{
+  mongoose.connect('mongodb://localhost/users_test', { useMongoClient: true })
+    .then((db)=>done())
+    .catch((err)=>done(err));
+});
 
 beforeEach((done) => {
   mongoose.connection.collections.users.drop(() => {
