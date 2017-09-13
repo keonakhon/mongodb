3c1d143dc73037bd3160b9411a87a1fce33d6cb3
Keo-PC\Keo <keonakhon.kawan@outlook.com>
2017-09-14 01:31:14 +0700

    update: require a user name

--- udemy-tutorial/01-users/test/validation_test.js
+++ udemy-tutorial/01-users/test/validation_test.js
@@ -3,6 +3,11 @@ const User = require('../src/user');
 
 describe('Validating records', () => {
   it('require a user name', () => {
+    const user = new User({ name: undefined });
+    const validationResult = user.validateSync();
+    //ES5 const message = validationResult.errors.name.message;
+    const { message } = validationResult.errors.name; //ES6
 
+    assert(message === 'Name is required.');
   });
 });
