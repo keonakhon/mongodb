be08e664b006cf6cf8154701a9209dc1613240bb
Keo-PC\Keo <keonakhon.kawan@outlook.com>
2017-09-13 14:03:26 +0700

    update: add helper function for assertation, which made code shorter

--- udemy-tutorial/01-users/test/update_test.js
+++ udemy-tutorial/01-users/test/update_test.js
@@ -10,15 +10,19 @@ describe('Update records', () => {
       .then(() => done());
   });
 
-  it('instance type using set and save', (done) => {
-    joe.set('name', 'Alex');
-    joe.save()
+  function assertName(operation, done) {
+    operation
       .then(() => User.find({}))
       .then((users) => {
         assert(users.length === 1);
         assert(users[0].name === 'Alex');
         done();
       });
+  }
+
+  it('instance type using set and save', (done) => {
+    joe.set('name', 'Alex');
+    assertName(joe.save(), done);
   });
 
 
