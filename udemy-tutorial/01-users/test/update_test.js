e3ede9d98b6431f008f03a9797bf971042de0a1d
Keo-PC\Keo <keonakhon.kawan@outlook.com>
2017-09-13 13:48:47 +0700

    update: add instance type using set and save

--- udemy-tutorial/01-users/test/update_test.js
+++ udemy-tutorial/01-users/test/update_test.js
@@ -10,4 +10,16 @@ describe('Update records', () => {
       .then(() => done());
   });
 
+  it('instance type using set and save', (done) => {
+    joe.set('name', 'Alex');
+    joe.save()
+      .then(() => User.find({}))
+      .then((users) => {
+        assert(users.length === 1);
+        assert(users[0].name === 'Alex');
+        done();
+      });
+  });
+
+
 });
