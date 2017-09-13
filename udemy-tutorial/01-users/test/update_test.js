ef45e8603fb88377caf8089737e7f7190a9b2f2b
Keo-PC\Keo <keonakhon.kawan@outlook.com>
2017-09-13 18:47:01 +0700

    update: The Increment Update Operator

--- udemy-tutorial/01-users/test/update_test.js
+++ udemy-tutorial/01-users/test/update_test.js
@@ -49,4 +49,13 @@ describe('Update records', () => {
       done
     );
   });
+
+  it('A user can have their postcount increment by 1', (done) => {
+    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.postCount === 1);
+        done()
+      });
+  });
 });
