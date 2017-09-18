d6a0cc119223b07954f1da2d4f9490173e189dd0
Keo-PC\Keo <keonakhon.kawan@outlook.com>
2017-09-19 02:26:39 +0700

    update: Adding Subdocuments to Existing Records

--- udemy-tutorial/01-users/test/subdocument_test.js
+++ udemy-tutorial/01-users/test/subdocument_test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Subdocuments', () => {
-  it('can create a subdocument', (done) => {
+  it('Can create a subdocument', (done) => {
     const joe = new User({
       name: 'Joe',
       posts: [{ title: 'PostTitle' }]
@@ -12,6 +12,25 @@ describe('Subdocuments', () => {
       .then(() => User.findOne({ name: 'Joe' }))
       .then((user) => {
         assert(user.posts[0].title === 'PostTitle');
+        done();
+      });
+  });
+
+  it('Can add subdocuments to an existing record', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: []
+    });
+
+    joe.save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        user.posts.push({ title: 'New Post' });
+        return user.save();
+      })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts[0].title === 'New Post');
         done();
       });
   });
