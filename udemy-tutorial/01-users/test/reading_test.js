48d68dcf5b5e95af78052a3c4723881d9c7d7296
Keo-PC\Keo <keonakhon.kawan@outlook.com>
2017-09-26 03:43:33 +0700

    update: add Writing Skip, Limit Queries and Sorting Collections

--- udemy-tutorial/01-users/test/reading_test.js
+++ udemy-tutorial/01-users/test/reading_test.js
@@ -2,12 +2,16 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Reading users out of the database', () => {
-  let joe;
+  let joe, alex, maria, zach;
 
   beforeEach((done) => {
-      joe = new User({ name: 'Joe' });
-      joe.save()
-        .then(() => done());
+    alex = new User({ name: 'Alex' });
+    joe = new User({ name: 'Joe' });
+    maria = new User({ name: 'Maria' });
+    zach = new User({ name: 'Zach'});
+
+    Promise.all([alex.save(), joe.save(), maria.save(), zach.save()])
+      .then(() => done());
   });
 
   it('find all users with a name of joe', (done) => {
@@ -23,6 +27,19 @@ describe('Reading users out of the database', () => {
     User.findOne({ _id: joe._id})
       .then((user) => {
         assert(user.name === 'Joe');
+        done();
+      });
+  });
+
+  it('can skip and limit the result set', (done) => {
+    User.find({})
+      .sort({ name: 1 }) // 1 start with letter A, -1 start with letter Z
+      .skip(1)
+      .limit(2)
+      .then((users) => {
+        assert(users.length === 2);
+        assert(users[0].name === 'Joe');
+        assert(users[1].name === 'Maria');
         done();
       });
   });
