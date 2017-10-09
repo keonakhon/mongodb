c27f377159cfe6acdddcb86193446d1ad8d2545c
Keo-PC\Keo <keonakhon.kawan@outlook.com>
2017-10-10 02:50:43 +0700

    add Testing a GeoNear Query

--- udemy-tutorial/03-muber/test/controllers/drivers_controller_test.js
+++ udemy-tutorial/03-muber/test/controllers/drivers_controller_test.js
@@ -52,4 +52,25 @@ describe('Drivers controller', () => {
         });
     });
   });
+
+  it('Get to /api/drivers find drivers in a location', done => {
+    const seattleDriver = new Driver({
+      email: 'seatle@test.com',
+      geomtery: { type: 'Point', coordinates: [-122.4759902, 47.6147628] }
+    });
+    const miamiDriver = new Driver({
+      email: 'miami@test.com',
+      geomtery: { type: 'Point', coordinates: [-80.253, 25.791] }
+    });
+
+    Promise.all([ seattleDriver.save(), miamiDriver.save() ])
+      .then(() => {
+        request(app)
+          .get('/api/drivers?lng=-80&lat=25')
+          .end((err, response) => {
+            console.log(response);
+            done();
+          });
+      });
+  });
 });
