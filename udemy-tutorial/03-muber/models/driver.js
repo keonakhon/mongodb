95046a61f2f442a50a7f14f3a110af507618da76
Keo-PC\Keo <keonakhon.kawan@outlook.com>
2017-10-09 15:39:27 +0700

    add The GeoJSON Schema

--- udemy-tutorial/03-muber/models/driver.js
+++ udemy-tutorial/03-muber/models/driver.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const PointSchema = new Schema({
+  type: { type: String, default: 'Point' },
+  coordinates: { type: [Number], index: '2dsphere' }
+});
+
 const DriverSchema = new Schema({
   email: {
     type: String,
